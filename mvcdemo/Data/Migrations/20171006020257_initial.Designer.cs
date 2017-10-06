c84837e079b4588054c8bf7c86560511cae70148
Harith <mharith.mtarik@eceos.com>
2017-10-06 17:54:27 +0800

    Adding Registration
    - Modified Registration page and login page
    - Change the connection string to SQL Server Express

--- mvcdemo/Data/Migrations/20171006020257_initial.Designer.cs
+++ mvcdemo/Data/Migrations/20171006020257_initial.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace mvcdemo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171006020257_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ mvcdemo/Models/profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcdemo.Models
+{
+    public class Profile
+    {
+        public int ID { get; set; }
+        [Required]
+        public string EmployeeNo { get; set; }
+        public string Position { get; set; }
+        public string Remarks { get; set; }
+    }
+}

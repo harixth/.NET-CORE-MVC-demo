918fb95690e65cd04506b44a5b8d5effe9940360
Harith <mharith.mtarik@eceos.com>
2017-10-23 10:07:17 +0800

    Add Hompage

--- mvcdemo/Controllers/HomeController.cs
+++ mvcdemo/Controllers/HomeController.cs
@@ -18,8 +18,6 @@ namespace mvcdemo.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
